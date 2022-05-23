d42c3f9b15a350a0b8843aa40da63dd5295a8047
Czw996 <459749926@qq.com>
2022-05-23 21:18:33 +0800

    feat: 调整

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,43 @@
 <template>
   <div id="app">
-    <a-config-provider :locale="locales.zh_CN">
+    <a-config-provider :locale="zh_CN">
       <router-view />
     </a-config-provider>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        locales: window.antd.locales,
-      };
-    },
-  };
+import zh_CN from "ant-design-vue/lib/locale-provider/zh_CN";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      zh_CN,
+    };
+  },
+};
 </script>
 
 <style>
-  html {
-    height: 100%;
-  }
+html {
+  height: 100%;
+}
 
-  body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    height: 100%;
-  }
-  .panel .ant-table {
-    min-height: 220px !important;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+.panel .ant-table {
+  min-height: 220px !important;
+}
 </style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,9 @@
 import Vuex from 'vuex'
 import user from './modules/user'
 import system from './modules/system'
+import Vue from 'vue'
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
