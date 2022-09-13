7788bfa2a7c9db31c59b883b8ce75031004c36d4
Czw996 <459749926@qq.com>
2022-09-13 21:07:17 +0800

    feat: 调整

--- admin/src/views/warehouse/inStock/inStockOrder.vue
+++ admin/src/views/warehouse/inStock/inStockOrder.vue
@@ -16,7 +16,7 @@
           <div slot="action" slot-scope="value, item">
             <a-button-group size="small">
               <a-button size="small" @click="detial(item)">详情</a-button>
-              <a-button type="primary" size="small" @click="toStockIn(item)">入库</a-button>
+              <a-button :disabled="item.is_void" type="primary" size="small" @click="toStockIn(item)">入库</a-button>
             </a-button-group>
           </div>
         </a-table>
